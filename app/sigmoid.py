6c124eb89bc3bdbfc36cd2ee9ae5a7c09c6568a4
kim1109123 <66085474+kim1109123@users.noreply.github.com>
2021-01-13 02:31:48 +0900

    Update sigmoid.py

--- app/sigmoid.py
+++ app/sigmoid.py
@@ -1,7 +1,13 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-x = np.arange(1,10)
-y = x*5
-plt.plot(x,y,'r')
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+weight = 1
+sigmoid_x = np.arange(-10, 10)
+sigmoid_y = sigmoid(weight * sigmoid_x)
+plt.plot(sigmoid_x, sigmoid_y, label='test1')
 plt.show()
